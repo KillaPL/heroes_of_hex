18cdf12bbc1bb28779caa23ebdc4b5c4c331c726
Killa <lukasz.strzebinczyk@gmail.com>
2015-12-14 22:12:14 +0100

    fix

--- lib/garbage_data_collector.rb
+++ lib/garbage_data_collector.rb
@@ -11,29 +11,27 @@ require 'active_support/core_ext/time/zones'
 require_relative "./../app/models/session"
 require_relative "./../app/models/room"
 
-environment = nil
+db_config = nil
 
 if ENV["HOME"] == "/home/deploy"
-  environment = 'production'
+  db_config = {
+    adapter: 'postgresql',
+    pool: 1,
+    timeout: 5000,
+    database: 'hoh_production',
+    host: 'localhost',
+    encoding: 'utf8',
+    username: ENV['HOH_DATABASE_USER'],
+    password: ENV['HOH_DATABASE_PASSWORD'],
+  }
 else
-  environment = 'development'
+  db_config = {
+    adapter: 'postgresql',
+    pool: 1,
+    timeout: 5000,
+    database: 'hoh_dev'
+  }
 end
-
-db_config       = YAML::load(File.open('config/database.yml'))[environment]
-db_config['pool'] = 1
-db_config['username'] = ENV['HOH_DATABASE_USER']
-db_config["password"] = ENV['HOH_DATABASE_PASSWORD']
-
-# => {"adapter"=>"postgresql",
-#  "pool"=>1,
-#  "timeout"=>5000,
-#  "database"=>"hoh_production",
-#  "host"=>"localhost",
-#  "encoding"=>"utf8",
-#  "username"=>"<%= ENV['HOH_DATABASE_USER'] %>",
-#  "password"=>"<%= ENV['HOH_DATABASE_PASSWORD'] %>"}
-
-# binding.pry
 
 ActiveRecord::Base.establish_connection(db_config)
 
